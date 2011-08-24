6b3e650dcf1259561354c6851093bd10a9d794c6
HighfiveYS <rtyua@hanmail.net>
2011-08-24 18:04:27 +0900

    toast input

---
+++ githubtestproj/src/ys/test/GithubtestprojActivity.java
@@ -0,0 +1,26 @@
+package ys.test;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.Toast;
+
+
+public class GithubtestprojActivity extends Activity {
+    /** Called when the activity is first created. */
+	
+	 Toast toast = Toast.makeText(this, "good", Toast.LENGTH_LONG);
+	
+	
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+        
+        toast.show();
+        
+    }
+    
+  
+   
+   
+}
